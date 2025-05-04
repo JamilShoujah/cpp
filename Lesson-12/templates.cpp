c642375c436f9f920f21d8614e4848a641f335af
JamilShoujah <jws09@mail.aub.edu>
2025-05-04 15:19:37 +0300

    fix

--- Lesson-12/templates.cpp
+++ Lesson-12/templates.cpp
@@ -5,19 +5,17 @@ using namespace std;
 // Templates in C++ are a powerful feature that allows functions and classes to operate with generic types. 
 // This means that you can write a single function or class to work with different data types, without duplicating the code. 
 // There are two types of templates in C++:
-    // Function Templates: Allow you to create a generic function that can handle different data types.
-    // Class Templates: Allow you to create a generic class that can handle different data types.
+    // 1. Function Templates
+    // 2. Class Templates
 
-// Function Templates:
-// A function template is defined by using the template keyword before the function definition. 
-// It lets you define a function without specifying the type of its arguments.
-// The function will be automatically instantiated for the appropriate types when it is called.
+// ==============================
+// FUNCTION TEMPLATES
+// ==============================
 
-// Syntax of Function Template
-
-template <typename T> // template <typename T> is the template declaration.
-T add(T a, T b) { // T is a placeholder for the data type.
-    // add(T a, T b) is the function definition that works with any data type T that supports the + operator.
+// A function template lets you define a generic function that works with any data type.
+// Syntax of Function Template:
+template <typename T>
+T add(T a, T b) {
     return a + b;
 }
 
@@ -28,70 +26,90 @@ void printData(T a, M b) {
 
 template <typename J, typename R>
 R specialAdd(J val1, R val2){
-    return val1 + val2
+    return val1 + val2; // This works as long as J + R is a valid expression
 }
 
+// ==============================
+// FUNCTION TEMPLATE SPECIALIZATION
+// ==============================
+
+// Sometimes, you want a special implementation for a particular type. For that, you can specialize the template.
+template <>
+string add<string>(string a, string b) {
+    return a + " (specialized) " + b;
+}
 
 void functionTemplateExample() {
     int x = 5, y = 10;
     double dx = 3.5, dy = 4.5;
-    string sx ="Hello ", sy ="World!";
-    // Here, the compiler automatically infers the type as 'int'
-    cout << "Sum of integers: " << add(x, y) << endl;
+    string sx = "Hello", sy = "World";
 
-    // Here, the compiler automatically infers the type as 'double'
-    cout << "Sum of doubles: " << add(dx, dy) << endl;
-
-    cout << add(sx, sy) <<endl;
+    cout << "Sum of integers: " << add(x, y) << endl;       // Uses general template
+    cout << "Sum of doubles: " << add(dx, dy) << endl;      // Uses general template
+    cout << "Sum of strings (specialized): " << add(sx, sy) << endl; // Uses specialized version
 }
 
-// Class Templates:
-// A class template allows you to create a class to handle various types, much like function templates but for 
-// entire classes. This is useful when you need a class that can store or operate on values of different types.
 
-// Syntax of Class Template
+// ==============================
+// CLASS TEMPLATES
+// ==============================
 
-template <typename T> // template <typename T> declares a template that works with a type T.
-class Boxs { // Boxs is a class that can store any type T.
-public:
-    T value;
-    Boxs(T val) : value(val) {}
-    T getValue() { return value; }
-};
-
-// example:
-// Class template definition
+// A class template allows a class to operate on multiple data types.
 template <typename T>
 class Box {
 private:
-    T value;   // The value stored in the box
+    T value;
 
 public:
-    // Constructor to initialize the value
     Box(T val) : value(val) {}
+    T getValue() { return value; }
+    void setValue(T val) { value = val; }
+};
 
-    // Getter function to return the stored value
-    T getValue() { 
-        return value; 
-    }
-    
-    // Setter function to update the value
-    void setValue(T val) {
-        value = val;
-    }
+// ==============================
+// CLASS TEMPLATE SPECIALIZATION
+// ==============================
+
+// Specialize the Box class for string type
+template <>
+class Box<string> {
+private:
+    string value;
+
+public:
+    Box(string val) : value(val) {}
+    string getValue() { return "Specialized Box: " + value; }
+    void setValue(string val) { value = val; }
 };
 
 void classTemplateExample() {
-    // Creating a Box of integer
-    Box<int> intBox(5);  // Here, Box<int> means T is replaced by int
+    Box<int> intBox(5);
     cout << "Integer Box Value: " << intBox.getValue() << endl;
 
-    // Creating a Box of double
-    Box<double> doubleBox(3.14);  // Here, Box<double> means T is replaced by double
+    Box<double> doubleBox(3.14);
     cout << "Double Box Value: " << doubleBox.getValue() << endl;
 
-    // Changing the value in the integer box
+    Box<string> strBox("Template");
+    cout << strBox.getValue() << endl; // Will use the specialized class version
+
     intBox.setValue(10);
     cout << "Updated Integer Box Value: " << intBox.getValue() << endl;
-
 }
+
+// ==============================
+// MAIN
+// ==============================
+
+int main() {
+    functionTemplateExample();
+    cout << "----------------------\n";
+    classTemplateExample();
+    return 0;
+}
+
+
+// Summary of Specialization:                      
+// Function:  
+    // template<typename T> T add(T a, T b) => specialization => template<> string add<string>(string a, b)
+// Class:
+    // template<typename T> class Box => specialization => template<> class Box<string>     
